9b6f7fc2027e76b59cb0df84b393d7c7fcf1d9fb
cmvn-thinhtran <ta642379@gmail.com>
2023-12-07 09:56:06 +0700

    add calling api for call staff fucntion

--- src/components/UI/call-staff/CallStaff.jsx
+++ src/components/UI/call-staff/CallStaff.jsx
@@ -1,23 +1,48 @@
 import { Button } from '@mui/material';
-import React, {useState} from 'react'
+import React, { useState } from 'react'
 
 import Snackbar from '@mui/material/Snackbar';
 import Alert from '@mui/material/Alert';
+import axios from "axios"
+
 const CallStaff = (props) => {
-  const [open, setOpen] = useState(false);
+    const [open, setOpen] = useState(false);
+    const [message, setMessage] = useState('Staff is comming')
+    const [buttonDisabled, setButtonDisabled] = useState(false);
 
     const handleOpen = () => {
         setOpen(true);
-      };
-    
-      const handleClose = () => {
+    };
+
+    const handleClose = () => {
         setOpen(false);
-      };
+    };
     const handleCallStaff = (e) => {
         e.stopPropagation()
-        handleOpen()
-        // TODO a request call-staff api here
-        console.log("called");
+        if(!buttonDisabled) {
+            setButtonDisabled(true);
+            handleOpen()
+            callStaff()
+            setTimeout(() => {
+                setButtonDisabled(false);
+            }, 60000); 
+        }
+    };
+    const callStaff = async () => {
+        try {
+            const responseCategory = await axios.get(
+              
+              `${process.env.REACT_APP_BE_URL}/api/call-staff/${sessionStorage.getItem('table_id')}`
+            );
+
+            if (responseCategory.status >= 200 && responseCategory.status < 300) {
+                setMessage("Staff is comming")
+            } else {
+                setMessage("Cannot call staff now")
+            }
+        } catch (err) {
+            setMessage("Cannot call staff now")
+        }
     };
     return (
         <>
@@ -26,7 +51,7 @@ const CallStaff = (props) => {
             </Button>
             <Snackbar open={open} autoHideDuration={2000} onClose={handleClose}>
                 <Alert onClose={handleClose} severity="success" sx={{ width: '100%' }}>
-                    Staff is comming
+                    {message}
                 </Alert>
             </Snackbar>
         </>
